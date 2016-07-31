875a47ee0097c055e1db2700b19cf4ba704a5c05
jerduar <jerduar@gmail.com>
2016-07-31 16:03:39 -0600

    Modificaciones

--- [MIA]Proyecto_201442819/main.c
+++ [MIA]Proyecto_201442819/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void CreacionDisco(int size, char path[256], char unidad[10], char nombre[256] ){
+
+    char destino[30];
+
+    strcpy(destino,path);
+    strcat(destino,nombre);
+
+    printf("%s",destino);
+
+    FILE *archivo_binario;
+    archivo_binario = fopen(destino,"wb+");
+    if(archivo_binario == NULL){
+        printf("ERRO AL CARGAR EL ARCHIVO\n");
+    }else{
+        printf("DISCO CREADO!\n");
+    }
+
+}
+
 int main()
 {
-    printf("Proyecto Archivos");
+    printf("Proyecto Archivos\n");
+    CreacionDisco(12,"/home/jerduar/","M","prueba.dsk");
     return 0;
 }
